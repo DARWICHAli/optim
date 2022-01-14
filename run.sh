58f704d500dc52650526db4e1c442465be9def5a
DARWICH ALI <adarwich@etu.unistra.fr>
2022-01-14 22:34:30 +0100

    modif gitignore && run.sh

--- run.sh
+++ run.sh
@@ -2,15 +2,16 @@
 
 suffix=".ez"
 foo=${1%"$suffix"}
-echo $foo
 # compile et exec
 if [ $# = 0 ] || [[ $1 != *".ez" ]]; then
     echo "Usage: ./run <file.ez> "
 else
+    echo "executing :" $1
     make easeaclean
     easena $1
     make
     ./$foo
+    echo "Done !"
     mv *.log logs/
 fi
 exit 0
